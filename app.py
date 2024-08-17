74a35c19b23a2a4c2037d930fb656c4cdf385110
THiyama <robot_toru@outlook.jp>
2024-08-17 18:56:18 +0900

    add user info

--- app.py
+++ app.py
@@ -4,8 +4,36 @@ from snowflake.snowpark import Session
 from utils.utils import create_session
 
 
+TEAMS = [
+    "",
+    "AccountAdmin",
+    "BusinessCritical",
+    "Cortex",
+    "DCR_DynamicTables",
+    "Enterprise_Edtion",
+    "Fail_Safe",
+    "Git",
+    "Horizon",
+    "Iceberg",
+    "JAROWINKLER_SIMILARITY",
+    "Kafka",
+    "Lineage",
+    "Marketplace",
+    "Notebooks",
+    "OrgAdmin",
+    "POLARIS",
+    "QualityMonitoring",
+    "ResouceMonitor",
+    "Snowpark",
+    "TrustCenter",
+    "UniversalSearch",
+    "VARCHAR",
+    "WAREHOUSE",
+    "XS",
+]
+
 st.title("top page")
-team_id = st.text_input("チーム名を入力してください")
+team_id = st.selectbox("チーム名を選択してください", options=TEAMS)
 if team_id:
     st.session_state.team_id = team_id
     st.session_state.snow_session = create_session(team_id)
